a5897257e718025520bb863d77210d6f7a1ac207
aimthiazz <aimthiazz@gmail.com>
2017-02-02 20:07:41 +0530

    Create Magnetic Design.py

---
+++ Magnetic Design.py
@@ -0,0 +1,10 @@
+#Magnetic Design
+#Author Imthiaz
+#Started 02-02-2017
+#Equations which calculate flux density on the motor
+#Inner rotor motor is assuemed
+Rs=input("Enter the stator radius") ## temporary
+Rr=input("Enter the rotor radius")
+Rm=input("Enter the radius at which the magnet ends")
+amc=input("Enter the manget fraction") ## Remember pole embrace in Maxwell tool
+n=1 ## Assume Fundamental component
