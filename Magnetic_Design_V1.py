ea145b97dd5cbae8e51e8ea268ffcf8f3d2ebcfd
aimthiazz <aimthiazz@gmail.com>
2017-02-05 21:03:28 +0530

    Update Magnetic_Design_V1.py

--- Magnetic_Design_V1.py
+++ Magnetic_Design_V1.py
@@ -5,7 +5,14 @@
 #Inner rotor motor is assuemed
 #involves simpler calculation
 import math
-Br=0.4
+import matplotlib.pyplot as plt
+Ns=int(input("Enter the Number of Slots"))
+Nm=int(input("Enter the Number of Poles"))
+Rs=float(input("Enter the stator radius")) ## temporary)
+Rr=float(input("Enter the rotor radius"))
+Rm=float(input("Enter the radius at which the magnet ends"))
+amc=float(input("Enter the manget fraction")) ## Remember pole embrace in Maxwell tool
+Br=float(input("Enter the Br"))
 kl=0.95
 kr=1.1
 pi=math.pi
@@ -17,5 +24,27 @@ urair=uo/uair
 Co=1
 Pc=lm/(g*Co)
 Bg=(kl*Co)*Br/(1+kr*(urair/Pc))
-g=[]
+g1=[]
+ksl=[]
 print(Bg)
+tetat=float(input("Enter the teeth angle"))
+tetas=float(input("Enter the slot angle"))
+#for i in range(0,int((tetas*100/2)-(tetat*100/2))+1):
+#    g1.append(1-((180/Nm)*(Rs/g)*(((float(i)/100)+tetas/2)+tetat/2)))
+#for j in range(0,int((tetas*100/2)-(tetat*100/2))+1):
+#    ksl.append((1+(lm/(g*urair)))/(g1[i]+(lm/(g*urair))))
+teta=[]
+teta.append(0)
+teta.append((tetas/2)-(tetat/2))
+teta.append(tetat)
+teta.append((tetas/2)-(tetat/2)+tetat)
+ksl.append(0)
+ksl.append(Bg)
+ksl.append(Bg)
+ksl.append(0)
+
+print(teta)
+print(ksl)
+plt.interactive(True)
+plt.plot(teta,ksl)
+input("Enter something")
